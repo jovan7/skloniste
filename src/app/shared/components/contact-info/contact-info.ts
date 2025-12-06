d0ec15d01fb5054a0c1c727138abdfbfab4eaaaa
jovanjonic <jovanjonic@gmail.com>
2025-12-06 19:05:00 +0100

    - email to clipboard

--- src/app/shared/components/contact-info/contact-info.ts
+++ src/app/shared/components/contact-info/contact-info.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {MatSnackBar} from '@angular/material/snack-bar';
+import {Snackbar} from '../snackbar/snackbar';
 
 @Component({
   selector: 'app-contact-info',
@@ -7,5 +9,14 @@ import { Component } from '@angular/core';
   standalone: true
 })
 export class ContactInfo {
+  private _snackBar = inject(MatSnackBar);
 
+  copyEmail(data: string) {
+    navigator.clipboard.writeText('adoptions@skloniste-pirot.org').then(() => {
+      this._snackBar.openFromComponent(Snackbar, {
+        data,
+        duration: 3000
+      });
+    });
+  }
 }
